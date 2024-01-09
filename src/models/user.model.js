0c3dbffc7e9ae38d18ce4421fbde0f48552b18e9
sadman <sadmanhossain400@gmail.com>
2024-01-09 17:46:35 +0600

    added user.model.js

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,
+        required: true,
+    }, 
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema);
+
+export {userSchema};
