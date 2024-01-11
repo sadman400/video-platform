c0db837e1838a0e3c0ab1b2f8f0f7a631ca36e1c
sadman <sadmanhossain400@gmail.com>
2024-01-11 22:22:45 +0600

    adding file uploading func

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!uploadOnCloudinary) return null
+
+        // upload the file 
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // it will remove the temporary file if the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
